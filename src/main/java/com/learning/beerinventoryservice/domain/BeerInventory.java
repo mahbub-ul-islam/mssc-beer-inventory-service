125fc8cae577984922af9ca7948b86db9e9b5dd9
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2023-04-25 16:07:24 +0600

    adding data model. closes #2

---
+++ src/main/java/com/learning/beerinventoryservice/domain/BeerInventory.java
@@ -0,0 +1,27 @@
+package com.learning.beerinventoryservice.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class BeerInventory extends BaseEntity {
+
+    @Builder
+    public BeerInventory(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate, UUID beerId,
+                         String upc, Integer quantityOnHand) {
+        super(id, version, createdDate, lastModifiedDate);
+        this.beerId = beerId;
+        this.upc = upc;
+        this.quantityOnHand = quantityOnHand;
+    }
+
+    private UUID beerId;
+    private String upc;
+    private Integer quantityOnHand = 0;
+}

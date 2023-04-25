8f1697d1d2eaaf106ca2729e8970f79ff0c95c46
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2023-04-25 16:20:19 +0600

    adding bootstrap dataloader. closes #4

---
+++ src/main/java/com/learning/beerinventoryservice/repositories/BeerInventoryRepository.java
@@ -0,0 +1,13 @@
+package com.learning.beerinventoryservice.repositories;
+
+import com.learning.beerinventoryservice.domain.BeerInventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface BeerInventoryRepository extends JpaRepository<BeerInventory, UUID> {
+
+    List<BeerInventory> findAllByBeerId(UUID beerId);
+
+}
